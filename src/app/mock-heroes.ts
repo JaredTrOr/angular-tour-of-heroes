4ef22b344e767dbcf55a7c583b78da7b11ae035a
JaredTrOr <trujillojaredalexander@gmail.com>
2024-01-17 00:24:57 -0600

    Creation of project and implementation of heroes component, hero interface and mock heroes file

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr. Nice', superpower: 'electric' },
+    { id: 13, name: 'Bombasto', superpower: 'fire' },
+    { id: 14, name: 'Celeritas', superpower: 'water' },
+    { id: 15, name: 'Magneta', superpower: 'water' },
+    { id: 16, name: 'RubberMan', superpower: 'dirt' },
+    { id: 17, name: 'Dynama', superpower: 'plant' },
+    { id: 18, name: 'Dr. IQ', superpower: 'ground' },
+    { id: 19, name: 'Magma', superpower: 'velocity' },
+    { id: 20, name: 'Tornado', superpower: 'air' }
+];
